b01a205f8570b36b2f25f672fd135761a33efe70
Saiju Lukose <saijulukose5592@gmail.com>
2023-12-17 12:22:32 +0200

    Script with echo command for practice...

---
+++ echo.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#######################################################
+# Script: 1_echo.sh
+# Description: A simple Bash script for practice echo commands
+# Author: Saiju Lukose
+#######################################################
+
+
+
+#!/bin/bash
+
+# This is an updated version of the Bash script for practice
+# It uses echo commands to print messages and variables with curly braces
+
+echo "Hello, this is your first Bash script!"
+echo "Welcome to the world of Bash scripting."
+
+# Variables
+name="Saiju Lukose"
+age=30
+
+# Using variables in echo with curly braces
+echo "My name is ${name} and I am ${age} years old."
+
+# Displaying a multiline message
+echo "This script demonstrates basic Bash scripting.
+It displays my name ${name} and age ${age}."
+
+# End of script
